866d4332c21056ed940caf4795f4d38fbb061037
Connor Wilson <wilso.conn@gmail.com>
2021-01-22 12:18:59 -0500

    basic webpack and react setup

---
+++ server/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}` );
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+};
