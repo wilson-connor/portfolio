1a64db7d3ef92c2d3f44347aecbf201d851aa966
Connor Wilson <wilso.conn@gmail.com>
2021-02-03 20:51:42 -0500

    added material ui, started building App bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, AppBar, Toolbar, Typography, makeStyles, useMediaQuery, useTheme, IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+
+
+const TopBarElements = ({ children, icon }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+  return matches ? children : icon;
+}
 
 const App = () => {
 
   return (
-    <div>Connor's Portfolio</div>
+    <div>
+    <AppBar position='sticky' color='default'>
+      <Toolbar>
+        <Typography variant="h6">Connor Wilson</Typography>
+        <TopBarElements
+          icon={
+          <IconButton edge="start">
+            <Menu/>
+          </IconButton>
+          }>
+          <Button>About</Button>
+          <Button>Projects</Button>
+          <Button>Contact</Button>
+        </TopBarElements>
+      </Toolbar>
+    </AppBar>
+      <Button variant="contained" color="primary">Hello World</Button>
+    </div>
   )
 }
 
