02598e4cfbda029cf646605affd264efd362caa3
Connor Wilson <wilso.conn@gmail.com>
2021-02-17 21:48:51 -0500

    set up express server for client-side routing

--- server/app.js
+++ server/app.js
@@ -6,7 +6,15 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/bundle.js'));
+})
+
+app.get('/*', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/index.html');
+  console.log(filePath);
+  res.sendFile(filePath);
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}` );
